d7a8c4a5b1ef7a9537aac71922883370992af5f4
YovkoGospodinov <yovko.gospodinov@gmail.com>
2017-10-30 23:34:55 +0200

    adding bg language

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index_eng');
+});
+
+app.get('/bg', function(request, response) {
+    response.render('pages/index_bg');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
